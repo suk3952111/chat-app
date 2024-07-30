60255479c73c748473d7b7d88a2aede17f9e2356
suk3952111 <suk3952111@naver.com>
2024-07-30 20:55:42 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://vwextkqnwkxzzjvkqoqt.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
